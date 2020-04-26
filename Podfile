4b7cf7a9b3c7993f49cc28a534c75644bacb46f7
Matthew Chung <m33chung@uwaterloo.ca>
2020-04-26 17:43:10 -0400

    add firebase

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TWO' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TWO
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestoreSwift'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Messaging'
+
+
+end
--- TWO/ViewController.swift
+++ TWO/ViewController.swift
@@ -7,27 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
+    var handle: AuthStateDidChangeListenerHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 26/255, alpha: 100)
         
-        let currentlyLabel = UILabel(frame: CGRect(x: 154, y: 53, width: 95, height: 25))
+        let currentlyLabel = UILabel(frame: CGRect.zero)
         currentlyLabel.textColor = .white
+        currentlyLabel.textAlignment = .center
         currentlyLabel.text = "currently:"
         currentlyLabel.font = UIFont(name: "Lato-Bold", size: 21)
+        currentlyLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(currentlyLabel)
+
+        NSLayoutConstraint.activate([
+            currentlyLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            currentlyLabel.centerYAnchor.constraint(equalTo: self.view.topAnchor, constant: 60)
+        ])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener{ (auth, user) in
+            
+        }
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
 
 }
 
